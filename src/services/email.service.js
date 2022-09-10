f16dac03337f2e4bf762e17f2f2cce5dbb003555
SOURABH SHARMA <sourabhsharma7662@gmail.com>
2022-09-10 11:43:56 +0530

    -

--- src/services/email.service.js
+++ src/services/email.service.js
@@ -1,21 +1,24 @@
-// const nodemailer = require("nodemailer");
-// const emailConfig = require("../config/email_config.json")[process.env.NODE_ENV || "development"];
+const nodemailer = require("nodemailer");
+const emailConfig = require("../config/mailer.json")[process.env.NODE_ENV || "development"];
 
-// let transporter = nodemailer.createTransport(emailConfig);
+let transporter = nodemailer.createTransport(emailConfig);
+const _SEND_EMAIL = async(emailOpts) => {
+    try {
+        let info = await transporter.sendMail({
+            from: `Renoguru <${emailConfig.auth.user}>`, // sender address
+            to: emailOpts.to, // list of receivers
+            subject: emailOpts.subject, // Subject line
+            text: "", // plain text body
+            html: emailOpts.html, // html body
+        });
+        console.warn("Email sent to ", emailOpts.to);
+        return info;
+    } catch (error) {
+        console.error("Email not sent ", error);
+        throw new Error(error);
+    }
+}
 
-// module.exports.sendEmail = async(emailOpts) => {
-//     try {
-//         let info = await transporter.sendMail({
-//             from: `Studeo <${emailConfig.auth.user}>`, // sender address
-//             to: emailOpts.to, // list of receivers
-//             subject: emailOpts.subject, // Subject line
-//             text: "", // plain text body
-//             html: emailOpts.html, // html body
-//         });
-//         console.log("Email sent to ", emailOpts.to);
-//         return info;
-//     } catch (error) {
-//         console.log("Email not sent ", error);
-//         throw new Error(error);
-//     }
-// }
+module.exports = {
+    _SEND_EMAIL
+}
