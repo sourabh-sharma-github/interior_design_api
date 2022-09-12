2664b8a518c772dcda7034d100f16a2558f5024c
SOURABH SHARMA <sourabhsharma7662@gmail.com>
2022-09-12 23:48:09 +0530

    https

--- src/server.js
+++ src/server.js
@@ -4,20 +4,24 @@ const db = require("./models");
 const pe = require("parse-error");
 const { port } = require('./config/index')['server']
 
-const server = http.createServer(app);
-// const https = require('https');
-// const fs = require('fs');
+// const server = http.createServer(app);
+const https = require('https');
+const fs = require('fs');
 
-// const options = {
-//   key: fs.readFileSync('key.pem'),
-//   cert: fs.readFileSync('cert.pem')
-// };
+var dad = fs.readFileSync('/etc/letsencrypt/live/renotopia.guru/fullchain.pem', 'utf8')
+var privateKey = fs.readFileSync('/etc/letsencrypt/live/renotopia.guru/privkey.pem', 'utf8')
+var certificate = fs.readFileSync('/etc/letsencrypt/live/renotopia.guru/cert.pem', 'utf8')
+var credentials = {
+  key: privateKey,
+  cert: certificate,
+  ca: dad
+};
 
-// const server = https.createServer(options, app);
+const server = https.createServer(credentials, app);
 
 db.sequelize
-.authenticate()
-.then(() => {
+  .authenticate()
+  .then(() => {
     console.log('connection has been established successfully.');
     db.sequelize.sync({
       logging: false,
@@ -25,15 +29,15 @@ db.sequelize
     })
   })
   .catch(err => console.error('unable to connect to the database', err));
-  
-  try {
-    server.listen(port, () => {
-      console.log(`🚀🚀 server started on port:: ${port} 🚀🚀`);
-    })
-  } catch (error) {
-    console.error("An error occured: ", error);
-  };
-  
-  process.on('unhandledRejection', error => {
-    console.error('Uncaught Error', pe(error));
-  });
+
+try {
+  server.listen(port, () => {
+    console.log(`🚀🚀 server started on port:: ${port} 🚀🚀`);
+  })
+} catch (error) {
+  console.error("An error occured: ", error);
+};
+
+process.on('unhandledRejection', error => {
+  console.error('Uncaught Error', pe(error));
+});
